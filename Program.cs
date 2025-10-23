87cc44b8efee0122ec9dfcf4efddf3de6b1243fb
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2025-10-23 20:54:09 +0530

    git push

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-using Dotnet8MySqlCrud.Data;
+﻿using Dotnet8MySqlCrud.Data;
 using Microsoft.EntityFrameworkCore;
+using DotNetEnv; // 👈 Add this
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 👇 Load .env file before configuration is built
+Env.Load();
+
+// 👇 Add environment variables to configuration
+builder.Configuration.AddEnvironmentVariables();
+
 // MySQL EF Core
 var conn = builder.Configuration.GetConnectionString("Default");
+// OR: var conn = Environment.GetEnvironmentVariable("ConnectionStrings__Default");
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     var serverVersion = ServerVersion.AutoDetect(conn);
