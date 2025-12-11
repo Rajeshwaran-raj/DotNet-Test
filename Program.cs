caf97258f8b2561e8cfc874c7aec090bed02d342
Rajeshwaran Murugesan <rajeshwaran.m@payorch.com>
2025-12-11 09:58:10 +0530

    Inmemory changes

--- Program.cs
+++ Program.cs
@@ -1,28 +1,8 @@
-﻿using Dotnet8MySqlCrud.Data;
-using Microsoft.EntityFrameworkCore;
-using DotNetEnv; // 👈 Add this
+﻿var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
-
-// 👇 Load .env file before configuration is built
-Env.Load();
-
-// 👇 Add environment variables to configuration
-builder.Configuration.AddEnvironmentVariables();
-
-// MySQL EF Core
-var conn = builder.Configuration.GetConnectionString("Default");
-// OR: var conn = Environment.GetEnvironmentVariable("ConnectionStrings__Default");
-
-builder.Services.AddDbContext<AppDbContext>(opt =>
-{
-    var serverVersion = ServerVersion.AutoDetect(conn);
-    opt.UseMySql(conn, serverVersion);
-});
-
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
 
 builder.Services.AddCors(opt =>
 {
@@ -36,6 +16,6 @@ app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("allow-all");
-app.MapControllers();
 
+app.MapControllers();
 app.Run();
